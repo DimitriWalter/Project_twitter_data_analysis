f0855030bf562edeae42641f2138f955e16761cf
Dimitri WALTER <dimitri.walter@ens.uvsq.fr>
2023-10-23 10:19:18 +0200

    changemnt de la fct liste hashtags

--- projet.py
+++ projet.py
@@ -16,13 +16,17 @@ for ligne in jeu_tweet :
 
 def liste_hashtags(tweet) : ## on prend le tweet en argument
     if "#" in tweet : ## Tout d'abord, on vérifie la présence d'hashtags dans la publication
-        hashtag = tweet.split("#") ## Si il y en a, alors on utilise .split("#") pour séparer les mots des hashtags
-        return hashtag[1:] ## On retourne la tweet à partir de l'indice 1 et non 0 car tweet[0] correspond au texte du tweet avant les hashtags
+        liste_h = [] ## initialise la liste qui va contenier les différents hashtags
+        split = tweet.split() ## Si il y en a, alors on utilise .split() pour séparer les mots du tweet
+        for elmnt in split : ## On parcoure tous les éléments
+            if "#" in elmnt : ## Si il y a un # dans un élément cela signifie que c'est un des hashtag du tweeet
+                liste_h.append(elmnt) ## J'ajoute alors mon élément contenant le hashtag à ma liste
+        return liste_h ## je retourne ma liste contenant la liste des hashtags
     else :
-        hashtag = []
-        return hashtag ## Si, au contraire il n'y en a pas, alors on renvoie une liste vide
-    
+        liste_h = []
+        return liste_h ## Si, au contraire il n'y en a pas, alors on renvoie une liste vide
 
+        
 ## Analyser le sentiment du tweet :
 
 def analys_feeling(tweet) :
