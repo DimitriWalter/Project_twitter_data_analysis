e59ed91efeb5002f7bf85d84a6cb46056873181a
Dimitri WALTER <dimitri.walter@ens.uvsq.fr>
2023-11-02 20:13:05 +0100

    update

--- projet.py
+++ projet.py
@@ -1,5 +1,4 @@
 from ast import Load
-import panda as pd
 import json
 from textblob import TextBlob
 
@@ -153,12 +152,14 @@ def topK_hashtags(k) :
     print(f"Voici le top {k} des hashtags qui reviennent le + souvent : \n")
     for i in range(k) :
         print(f"- {i+1}) {top[i][0]} avec {top[i][1]} occurences.") ## On affiche le top K des hashtags avec le nombres d'occurences à chaque fois
+
 '''
 
 k = int(input("Donnes moi un nombre k afin que je t'affiche le top k des hashtags : "))
 topK_hashtags(k)
 
 '''
+
 
 ## TopK des mentions :
 
@@ -175,10 +176,11 @@ dicK_mentions = dict(sorted(dicK_mentions.items(), key = lambda item : item[1], 
 def topK_mentions(k) :
     top = list(dicK_mentions.items()) ## On créé une liste contenant les items du dictionnaire
     while k > len(top) : ## On teste si la valeur k donnée n'est pas supérieur au nombre de mentions que l'on a
-        int(input(f"Désolé je ne peux pas t'imprimer le top {k} des mentions car il n'y en a que {len(top)}, donne moi un nombre + petit : ")) ## Si k supérieur au nombre de mentions alors on renvoie un message d'erreur et on demande au user d'en rentrer un autre
+        k = int(input(f"Désolé je ne peux pas t'imprimer le top {k} des mentions car il n'y en a que {len(top)}, donne moi un nombre + petit : ")) ## Si k supérieur au nombre de mentions alors on renvoie un message d'erreur et on demande au user d'en rentrer un autre
     print(f"Voici le top {k} des mentions qui reviennent le + souvent : \n")
     for i in range(k) :
         print(f"- {i+1}) {top[i][0]} avec {top[i][1]} occurences.") ## On affiche le top K des mentions avec le nombres d'occurences à chaque fois
+
 
 '''
 
@@ -186,4 +188,3 @@ k = int(input("Donnes moi un nombre k afin que je t'affiche le top k des mention
 topK_mentions(k)
 
 '''
-
