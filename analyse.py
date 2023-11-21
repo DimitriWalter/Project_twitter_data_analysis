f4ff1e2991f96190190c56e03cc193d2924ff079
Dimitri WALTER <dimitri.walter@ens.uvsq.fr>
2023-11-21 19:35:53 +0100

    update analyse

--- analyse.py
+++ analyse.py
@@ -24,7 +24,7 @@ def topK_hashtags():
     top = list(dicK_hashtags.items())  # On créé une liste contenant les items du dictionnaire
     while k > len(top):  # On teste si la valeur k donnée n'est pas supérieur au nombre d'hashtags que l'on a
         k = int(input(f"Désolé je ne peux pas t'imprimer le top {k} des hashtags car il n'y en a que {len(top)}, donne moi un nombre + petit : "))  # Si k supérieur au nombre d'hashtags alors on renvoie un message d'erreur et on demande au user d'en rentrer un autre
-    print(f"Voici le top {k} des hashtags qui reviennent le + souvent : \n")
+    print(f"\nTop {k} des hashtags qui reviennent le + souvent : \n")
     for i in range(k):
         print(f"- {i+1}) {top[i][0]} apparait {top[i][1]} fois.")  # On affiche le top K des hashtags avec le nombres d'occurences à chaque fois
 
@@ -53,7 +53,7 @@ def topK_mentions():
     top = list(dicK_mentions.items())  # On créé une liste contenant les items du dictionnaire
     while k > len(top):  # On teste si la valeur k donnée n'est pas supérieur au nombre de mentions que l'on a
         k = int(input(f"Désolé je ne peux pas t'imprimer le top {k} des mentions car il n'y en a que {len(top)}, donne moi un nombre + petit : "))  # Si k supérieur au nombre de mentions alors on renvoie un message d'erreur et on demande au user d'en rentrer un autre
-    print(f"Top {k} des mentions qui reviennent le + souvent : \n")
+    print(f"\nTop {k} des mentions qui reviennent le + souvent : \n")
     for i in range(k):
         print(f"- {i+1}) {top[i][0]} apparait {top[i][1]} fois.")  # On affiche le top K des mentions avec le nombres d'occurences à chaque fois
 
@@ -81,7 +81,7 @@ def topk_users():
     top = list(dicK_users.items())  # On créé une liste contenant les items du dictionnaire
     while k > len(top):  # On teste si la valeur k donnée n'est pas supérieur au nombre de mentions que l'on a
         k = int(input(f"Désolé je ne peux pas t'imprimer le top {k} des utilisateurs car il n'y en a que {len(top)}, donne moi un nombre + petit : "))  # Si k supérieur au nombre de mentions alors on renvoie un message d'erreur et on demande au user d'en rentrer un autre
-    print(f"Top {k} des utilisateurs qui reviennent le + souvent : \n")
+    print(f"\nTop {k} des utilisateurs qui reviennent le + souvent : \n")
     for i in range(k):
         print(f"- {i+1}) {top[i][0]} apparait {top[i][1]} fois.")  # On affiche le top K des mentions avec le nombres d'occurences à chaque fois
 
